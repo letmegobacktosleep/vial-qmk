59b0d434c65259651daf020ace965d8bc3907b41
letmegobacktosleep <winstonkwan06@gmail.com>
2024-11-11 00:00:15 +1100

    added dynamic tapping term and changed mouse acceleration

---
+++ keyboards/letmesleepsplit/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* Change Mouse acceleration */
+#define MOUSEKEY_INERTIA
+#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_TIME_TO_MAX 32
+#define MOUSEKEY_MAX_SPEED 48
+#define MOUSEKEY_FRICTION 24
+#define MOUSEKEY_MOVE_DELTA 1
+
+/* Change Tapping Term increment */
+#define TAPPING_TERM 200
+#define DYNAMIC_TAPPING_TERM_INCREMENT 50
