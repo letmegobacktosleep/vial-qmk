0706d1e102b01f2f8338a175fa42a1e3162bb83f
letmegobacktosleep <winstonkwan06@gmail.com>
2024-11-22 18:47:30 +1100

    added keymap with longer debounce

---
+++ keyboards/letmesleepsplit/keymaps/vial-10ms-debounce/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x92, 0x87, 0xA2, 0xAD, 0xE4, 0x04, 0xA0, 0x9A}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
+
+/* Increase debounce */
+#define DEBOUNCE 10
