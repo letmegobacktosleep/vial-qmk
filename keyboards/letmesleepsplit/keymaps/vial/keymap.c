1bafab5c2a9e73318c068d1d38a262b59aabad19
letmegobacktosleep <winstonkwan06@gmail.com>
2024-11-26 18:14:12 +1100

    moved back to keymap.c the location does not seem to be the issue

--- keyboards/letmesleepsplit/keymaps/vial/keymap.c
+++ keyboards/letmesleepsplit/keymaps/vial/keymap.c
@@ -97,3 +97,51 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 
 };
 #endif
+
+
+
+// caps lock = white
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 2, HSV_WHITE}, // starting at 1st LED, affecting 2 LEDs in the chain, makes it white.
+);
+
+// layer 1
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 1, HSV_MAGENTA},
+);
+
+// layer 2
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 1, HSV_CORAL},
+);
+
+// layer 3
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 1, HSV_CHARTREUSE},
+);
+
+// Define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer,
+    my_layer1_layer,    // Overrides caps lock layer
+    my_layer2_layer,    // Overrides other layers
+    my_layer3_layer     // Overrides other layers
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+/* make the leds do something */
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _LAYER3));
+    return state;
+}
