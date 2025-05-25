751c2fbc0a79e54b9659e894f9eb4b14fa0ef538
letmegobacktosleep <winstonkwan06@gmail.com>
2025-05-26 03:02:53 +1000

    add new keyboard but rgb isn't done, need to reorder

---
+++ keyboards/letmegobacktosleep/le_chiffre_rgb/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5F, 0xA9, 0x56, 0x6C, 0x9F, 0xCF, 0x28, 0xEA}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
